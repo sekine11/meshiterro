89ea7d2983290b8206d8dcbaca5d55aef34f1b67
sekine11 <sekine11665@gmail.com>
2020-03-05 19:09:27 +0900

    [Finish] 5-6

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   	resources :post_comments, only: [:create, :desstroy]
   end
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 
 end
