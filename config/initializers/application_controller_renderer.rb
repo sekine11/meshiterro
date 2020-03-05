745161d05349ce6d704fb5425d8c281ca189b6ee
sekine11 <sekine11665@gmail.com>
2020-03-05 16:28:42 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5fadb6c9f13188c9a99b4c1772f145e4d21571bb4a8b35b5b0402bc7ab6b1e07983a6c2978805374041f508082dc55454daf39a0e200ef1360ceb8483fe6bb76'
